7a2d86c4aae30d60ab91ad60c6eac805ae97b673
abdelrahman <abdelrahmanobo12@gmail.com>
2026-07-16 04:44:12 +0300

    feat: add generated dependency injection configuration file

---
+++ lib/config/di/di_config.config.dart
@@ -0,0 +1,48 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// dart format width=80
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:dio/dio.dart' as _i361;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i558;
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
+import 'package:shared_preferences/shared_preferences.dart' as _i460;
+
+import '../../core/cubits/locale/locale_cubit.dart' as _i273;
+import '../helpers/shared_preferences/shared_preferences_helper.dart' as _i425;
+import 'di_module.dart' as _i211;
+
+extension GetItInjectableX on _i174.GetIt {
+  // initializes the registration of main-scope dependencies inside of GetIt
+  Future<_i174.GetIt> init({
+    String? environment,
+    _i526.EnvironmentFilter? environmentFilter,
+  }) async {
+    final gh = _i526.GetItHelper(this, environment, environmentFilter);
+    final diModule = _$DiModule();
+    await gh.factoryAsync<_i460.SharedPreferences>(
+      () => diModule.sharedPreferences(),
+      preResolve: true,
+    );
+    gh.singleton<_i361.Dio>(() => diModule.dio());
+    gh.lazySingleton<_i558.FlutterSecureStorage>(
+      () => diModule.secureStorage(),
+    );
+    gh.factory<_i425.SharedPreferencesHelper>(
+      () => _i425.SharedPreferencesHelper(gh<_i460.SharedPreferences>()),
+    );
+    gh.factory<_i273.LocaleCubit>(
+      () => _i273.LocaleCubit(gh<_i425.SharedPreferencesHelper>()),
+    );
+    return this;
+  }
+}
+
+class _$DiModule extends _i211.DiModule {}
