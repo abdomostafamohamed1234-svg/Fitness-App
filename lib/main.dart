cd1cd516864ec7ba552098f1a44a7ae55ee5f0b4
abdelrahman <abdelrahmanobo12@gmail.com>
2026-07-16 05:02:18 +0300

    refactor: remove unused import for TestScreen widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flowery/config/routing/routing_generator.dart';
 import 'package:flowery/core/cubits/locale/locale_cubit.dart';
 import 'package:flowery/core/cubits/locale/locale_state.dart';
 import 'package:flowery/core/theme/app_theme.dart';
-import 'package:flowery/core/widgets/test_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
