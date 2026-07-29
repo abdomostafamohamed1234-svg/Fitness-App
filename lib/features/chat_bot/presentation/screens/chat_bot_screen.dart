ac1612db390389868540145b3523753d64e35cca
abdelrahman <abdelrahmanobo12@gmail.com>
2026-07-30 00:47:34 +0300

    feat: refactor chat bot screen to use ChatDrawer widget for improved code organization

--- lib/features/chat_bot/presentation/screens/chat_bot_screen.dart
+++ lib/features/chat_bot/presentation/screens/chat_bot_screen.dart
@@ -1,13 +1,11 @@
 import 'dart:ui';
 import 'package:flowery/config/l10n/translations/app_localizations.dart';
-import 'package:flowery/config/routing/routing_extensions.dart';
-import 'package:flowery/core/theme/app_colors.dart';
 import 'package:flowery/features/chat_bot/presentation/assets/chat_bot_assets_navigation.dart';
 import 'package:flowery/features/chat_bot/presentation/view_model/cubit/chat_bot_cubit.dart';
-import 'package:flowery/features/chat_bot/presentation/view_model/events/chat_bot_events.dart';
 import 'package:flowery/features/chat_bot/presentation/view_model/state/chat_bot_state.dart';
 import 'package:flowery/features/chat_bot/presentation/widgets/chat.dart';
 import 'package:flowery/features/chat_bot/presentation/widgets/chat_bot_screen_header.dart';
+import 'package:flowery/features/chat_bot/presentation/widgets/chat_drawer.dart';
 import 'package:flowery/features/chat_bot/presentation/widgets/chat_input_field.dart';
 import 'package:flowery/features/chat_bot/presentation/widgets/chat_welcome.dart';
 import 'package:flutter/material.dart';
@@ -37,96 +35,10 @@ class _ChatBotScreenState extends State<ChatBotScreen> {
     return BlocConsumer<ChatBotCubit, ChatBotState>(
       builder: (context, state) {
         return Scaffold(
-          endDrawer: Drawer(
-            backgroundColor: AppColors.borderColor.withValues(alpha: 0.75),
-            width: MediaQuery.of(context).size.width * 0.7,
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 10,
-                    vertical: 30,
-                  ),
-                  child: Text(
-                    localizations.previous_conversations,
-                    style: textTheme.headlineMedium,
-                    textAlign: TextAlign.right,
-                  ),
-                ),
-                Column(
-                  children:
-                      state.chats
-                          ?.map(
-                            (chat) => Column(
-                              children: [
-                                InkWell(
-                                  onTap: () {
-                                    context.read<ChatBotCubit>().doEvent(
-                                      SelectChatEvent(
-                                        chatIndex: state.chats!.indexOf(chat),
-                                      ),
-                                    );
-                                    context.pop();
-                                  },
-                                  child: SizedBox(
-                                    height: 30,
-                                    child: Row(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceEvenly,
-                                      children: [
-                                        const Icon(
-                                          Icons.arrow_back_ios_new,
-                                          size: 15,
-                                          color: AppColors.primaryColor,
-                                        ),
-
-                                        Row(
-                                          children: [
-                                            SizedBox(
-                                              width:
-                                                  MediaQuery.of(
-                                                    context,
-                                                  ).size.width *
-                                                  0.6,
-                                              child: Text(
-                                                chat.chatTitle,
-                                                maxLines: 1,
-                                                overflow: TextOverflow.ellipsis,
-                                              ),
-                                            ),
-                                            SizedBox(
-                                              width:
-                                                  MediaQuery.of(
-                                                    context,
-                                                  ).size.width *
-                                                  0.02,
-                                            ),
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                                const Padding(
-                                  padding: EdgeInsets.symmetric(
-                                    horizontal: 8.0,
-                                  ),
-                                  child: Divider(
-                                    thickness: 1,
-                                    color: AppColors.borderColor,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          )
-                          .toList() ??
-                      [],
-                ),
-              ],
-            ),
+          endDrawer: ChatDrawer(
+            localizations: localizations,
+            textTheme: textTheme,
+            state: state,
           ),
           body: LayoutBuilder(
             builder: (context, constraints) {
