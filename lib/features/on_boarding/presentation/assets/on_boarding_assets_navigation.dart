1e374460df5555ff8de872faf5e405adf43b7463
tadroussamirhanna-rgb <tadroussamirhanna@gmail.com>
2026-08-01 15:15:05 +0300

    edit profile feature

--- lib/features/on_boarding/presentation/assets/on_boarding_assets_navigation.dart
+++
@@ -1,6 +0,0 @@
-abstract class OnBoardingAssetsNavigation {
-  static const String background = "assets/on_boarding_background.jpg";
-  static const String pose1 = "assets/on_boarding_1.png";
-  static const String pose2 = "assets/on_boarding_2.png";
-  static const String pose3 = "assets/on_boarding_3.png";
-}
--- lib/features/on_boarding/presentation/view_model/events/on_boarding_events.dart
+++
@@ -1,6 +0,0 @@
-sealed class OnBoardingEvents {}
-
-class ChangePageEvent extends OnBoardingEvents {
-  final int pageNumber;
-  ChangePageEvent({required this.pageNumber});
-}
